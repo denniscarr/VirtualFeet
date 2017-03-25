0e7b0e4954301b64af86143e96495284d21743fc
Chaojin <jinchaoonly@gmail.com>
2017-03-25 13:13:29 -0400

    Piano chess board level (not finish yet)

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OncollisonEnter(Collision coll){
+		
+		if (coll.gameObject.tag == "Player2") {
+
+			this.GetComponent<AudioSource>().Play();
+
+		}
+	}
+}
