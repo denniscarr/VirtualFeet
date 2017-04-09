b67d884e13832fa7a7b7b0618cba8ad34ad4f2c8
denniscarr <dcarr1@gmail.com>
2017-04-09 18:24:41 -0400

    More work on Dennis' new level.

---
+++ Assets/LightFlicker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+    Light myLight;
+    Material mat;
+
+    [SerializeField] float flickerThreshold = 0.6f;
+    [SerializeField] float noiseSpeed = 0.01f;
+    float noiseTime = 0f;
+
+    [SerializeField] float flickerIntensity = 0.5f;
+    float originalIntensity;
+    Color originalColor;
+
+	void Start ()
+    {
+        myLight = GetComponent<Light>();
+        mat = GetComponentInParent<MeshRenderer>().material;
+        originalColor = mat.color;
+
+        originalIntensity = myLight.intensity;
+	}
+	
+	void Update ()
+    {
+        noiseTime += noiseSpeed;
+        float noise = Mathf.PerlinNoise(noiseTime, 0);
+
+        Debug.Log(noise);
+
+        if (noise > flickerThreshold)
+        {
+            myLight.intensity = flickerIntensity + Random.Range(-0.2f, 0.2f);
+            mat.SetColor("_EmissionColor", Color.gray);
+        }
+
+        else
+        {
+            myLight.intensity = originalIntensity;
+            mat.SetColor("_EmissionColor", originalColor);
+        }
+    }
+}
---
+++ Assets/LightPulse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPulse : MonoBehaviour {
+
+    Light myLight;
+    [SerializeField] float intensityMin = 1;
+    [SerializeField] float intensityMax = 20;
+
+	void Start ()
+    {
+        myLight = GetComponent<Light>();
+	}
+	
+	void Update ()
+    {
+        myLight.intensity = MyMath.Map(Mathf.Sin(Time.time), -1, 1, intensityMin, intensityMax);
+	}
+}
