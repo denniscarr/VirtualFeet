fd9eae44b16fd72c17022bef25279920f51c8f56
Chaojin <jinchaoonly@gmail.com>
2017-04-10 17:55:04 -0400

    Add new level

---
+++ Assets/Scripts/LineRender.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRender : MonoBehaviour {
+
+	public LineRenderer linerenderer;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		linerenderer = GetComponent<LineRenderer> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			
+	}
+
+	void OnTriggerEnter(Collider col){
+		
+		if (col.gameObject.tag == "Player") {
+			Debug.Log ("aaa");
+			linerenderer.SetPosition (0, this.gameObject.transform.position);
+			linerenderer.SetPosition (1, col.gameObject.transform.position);
+		}
+
+	}
+}
