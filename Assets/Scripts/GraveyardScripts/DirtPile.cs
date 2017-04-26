85c24fc874c8a5c2a19847bdfd003cd0045b9469
DavidFrisch <dmf463@nyu.edu>
2017-04-26 16:53:03 -0400

    added didrtKicking sound in Graveyard level

--- Assets/Scripts/GraveyardScripts/DirtPile.cs
+++ Assets/Scripts/GraveyardScripts/DirtPile.cs
@@ -7,11 +7,14 @@ public class DirtPile : Skulls {
     public Vector3 dir;
     public GameObject pile;
     public Vector3 dirtMod;
+    public AudioClip dirtKick;
+    AudioSource audio;
 
 	// Use this for initialization
 	void Start () {
 
         name = "Dirt Pile";
+        audio = GetComponent<AudioSource>();
 	}
 
     public override void XOnCollisionEnter(Collision other)
@@ -20,7 +23,7 @@ public class DirtPile : Skulls {
         GameObject dirt = Instantiate(Resources.Load("Dirt")) as GameObject;
         dirt.GetComponent<Transform>().localPosition = pile.transform.localPosition + dirtMod;
         dirt.GetComponent<Rigidbody>().velocity = dir;
-
+        audio.PlayOneShot(dirtKick);
     }
 
     // Update is called once per frame
