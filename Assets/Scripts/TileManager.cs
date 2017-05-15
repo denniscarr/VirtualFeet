8235d8ae96b560392c0a3d4415e419a6aca31886
Chaojin <jinchaoonly@gmail.com>
2017-05-15 19:19:37 -0400

    Don't cover my changes again!!

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -5,33 +5,54 @@ using UnityEngine;
 public class TileManager : MonoBehaviour {
 
 	public GameObject gamemanager;
+	public AudioClip thisClip;
 	private Color originalColor;
+
+	public bool ismoving;
+	public bool reset;
 
 
 	// Use this for initialization
 	void Start () {
 
+		ismoving = false;
+		reset = false;
+
 		gamemanager = GameObject.Find ("Game Manager"); 
+
+		thisClip = this.gameObject.GetComponent<AudioSource> ().clip;
+
+		originalColor = this.GetComponent<Renderer> ().material.GetColor ("_EmissionColor");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (ismoving == true) {
+
+			this.gameObject.transform.position = new Vector3(transform.position.x, -0.24f, transform.position.z);
+		}
+
+		if (reset == true) {
+
+			this.gameObject.GetComponent<Renderer>().material.SetColor("_EmissionColor", originalColor);
 		
+			reset = false;
+
+		}
 	}
 
 	void OnTriggerEnter(Collider coll){
 		
 		if (coll.gameObject.tag == "Player") {
 
-			this.GetComponent<AudioSource>().Play();
+			this.GetComponent<AudioSource>().PlayOneShot(thisClip,1f);
 
 			Debug.Log (this.GetComponent<AudioSource> ().clip.name);
 
 			gamemanager.GetComponent<TileLevelManager> ().playersteps.Add(this.GetComponent<AudioSource> ().clip.name);
 
-			originalColor = this.GetComponent<Renderer>().material.GetColor("_EmissionColor");
-
-			this.GetComponent<Renderer> ().material.SetColor("_EmissionColor",Color.yellow);
+			ismoving = true;
 
 			StartCoroutine (Wait());
 
@@ -43,7 +64,9 @@ public class TileManager : MonoBehaviour {
 			
 			yield return new WaitForSeconds(1);
 
-			this.GetComponent<Renderer> ().material.SetColor("_EmissionColor",originalColor);
+			ismoving = false;
+
+			this.gameObject.transform.position = new Vector3(transform.position.x,0f, transform.position.z);
 
 
 	}
